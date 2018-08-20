b98a665a2ccbcf40e6a10eed5d182a5d4690ffc3
ahuigo <a132811@gmail.com>
2018-08-20 19:39:35 +0800

    vue-rollup-todo

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,25 @@
-// rollup.config.js
-import json from 'rollup-plugin-json';
+import json from "rollup-plugin-json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import VuePlugin from "rollup-plugin-vue";
 
-export default {
-  input: 'src/main.js',
-  output: {
-    file: 'bundle.js',
-    format: 'cjs'
-  },
-  plugins: [ json() ]
+export default{
+    //input: "src/main.js",
+    input: ["src/index.js"],
+	output: {
+        format: "esm",
+        name: "myBundle",
+        dir: "dist",
+        //file: "dist/bundle.js",
+        paths: {
+            d3: "https://d3js.org/d3.v4.min",
+            vue: "https://cdn.bootcss.com/vue/2.5.17-beta.0/vue.esm.browser.js",
+        }
+
+	},
+    experimentalCodeSplitting: true,
+    external: ["lodash", "d3","vue"],
+    plugins:[json(), resolve(), VuePlugin() ],
+    //plugins:[VuePlugin() ],
 };
--- src/main2.js
+++
@@ -1,6 +0,0 @@
-// src/main2.js
-export default function () {
-  return import('./main.js').then(({ default: main }) => {
-    main();
-  });
-}
